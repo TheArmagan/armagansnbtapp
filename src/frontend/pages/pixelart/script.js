9e7661458be958fc578da864915f7c949db3bb4d
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2021-03-01 15:37:44 +0300

    some cool updates on pixelart generator screen

--- src/frontend/pages/pixelart/script.js
+++ src/frontend/pages/pixelart/script.js
@@ -5,27 +5,37 @@ var componentScript = {
       outputFile: null,
       imageWidth: 0,
       imageHeight: 0,
-      imagePixelAmount: 0
+      imagePixelAmount: 0,
+      scaleFactor: 1,
+      ditheringFactor: 5
     }
   },
   watch: {
-    async inputFile(file) {
-      if (!file?.name) {
+    async inputFile() {
+      this.calcluteImageSize();
+    },
+    outputFile(file) {
+      if (!file?.name) return;
+      if (file.size != 0) {
+        NOTIFY.show("Please select a clean txt file.");
+      }
+    },
+    scaleFactor() {
+      this.calcluteImageSize();
+    }
+  },
+  methods: {
+    async calcluteImageSize() {
+      if (!this.inputFile?.name) {
         this.imageWidth = 0;
         this.imageHeight = 0;
         this.imagePixelAmount = 0;
         return;
       }
-      const { width, height, pixelAmount } = await API.getImageSize(file.path);
-      this.imageWidth = width;
-      this.imageHeight = height;
-      this.imagePixelAmount = pixelAmount;
-    },
-    async outputFile(file) {
-      if (!file?.name) return;
-      if (file.size != 0) {
-        NOTIFY.show("Please select a clean txt file.");
-      }
+      const { width, height } = await API.getImageSize(this.inputFile.path);
+      this.imageWidth = Math.round(width * this.scaleFactor);
+      this.imageHeight = Math.round(height * this.scaleFactor);
+      this.imagePixelAmount = Math.round(this.imageWidth * this.imageHeight);
     }
   }
 }
