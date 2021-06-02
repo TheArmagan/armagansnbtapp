b1fcea147c02c97a37ffcc78b5b4ae71f27d815a
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2021-06-03 00:40:50 +0300

    Update Appender.js

--- src/utilities/Appender.js
+++ src/utilities/Appender.js
@@ -3,15 +3,17 @@ class Appender {
   path = "";
   limit = 2048;
   list = [];
-  constructor(path = "", limit = 2048) {
+  joiner = "";
+  constructor(path = "", limit = 2048, joiner="") {
     this.path = path;
     this.limit = limit;
+    this.joiner = joiner;
   }
 
   append(data = "", force = false) {
     this.list.push(data);
     if (this.list.length >= this.limit || force) {
-      fs.appendFileSync(this.path, this.list.join(""), "utf-8");
+      fs.appendFileSync(this.path, this.list.join(this.joiner), "utf-8");
       this.list.length = 0;
     }
   }
